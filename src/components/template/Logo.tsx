87c73ba7aad956a606d6d0627d694a9078b25246
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-20 14:58:57 -0300

    chore: organização do código

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -5,7 +5,11 @@ import Link from "next/link"
 export default function Logo() {
   return (
     <Link href="/">
-      <div className="flex flex-col items-center mt-2">
+      <div
+        className="
+      flex flex-col items-center 
+      mt-2"
+      >
         {/* Texto "A Z" */}
         <div className="text-xl leading-4">A Z</div>
         {/* Ícone */}
