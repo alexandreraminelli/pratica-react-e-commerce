4618c288798dbf28bc60e51f15b42db0d979072a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-20 15:13:12 -0300

    feat: estilização do <main> (conteúdo principal da página)

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -11,11 +11,20 @@ export interface PaginaProps {
 /** Componente com o template de uma página. */
 export default function Pagina(props: PaginaProps) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
       {/* Cabeçalho */}
       <Cabecalho />
       {/* Elemento filho (conteúdo da página) */}
-      <main>{props.children}</main>
+      <main
+        className={`
+      w-[90vw] w-[1200px] max-w-[1200px] mx-auto
+      flex-1
+      py-10
+      ${props.className}
+      `}
+      >
+        {props.children}
+      </main>
     </div>
   )
 }
